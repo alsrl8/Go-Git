9b48b04556387bac223e4e16e4c174d17dc1021f
alsrl8 <alsrl8@naver.com>
2024-04-04 10:27:52 +0900

    프로그램 종료 후 사용자 입력을 받도록 대기

--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"Go-Git/config"
 	"Go-Git/core"
+	"Go-Git/terminal"
+	"bufio"
+	"os"
 )
 
 func main() {
 	configFilename := "./gogit.config"
 	conf := config.SetConfig(configFilename)
 	core.Service(conf)
+
+	terminal.PrintNotice("Press Enter to exit...")
+	reader := bufio.NewReader(os.Stdin)
+	_, _ = reader.ReadString('\n')
 }
